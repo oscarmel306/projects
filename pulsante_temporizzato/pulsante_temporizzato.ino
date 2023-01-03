1e707c611fde84108793657fe513684e604640b6
Oscar Mel <121770935+oscarmel306@users.noreply.github.com>
2023-01-03 18:20:30 +0100

    Update pulsante_temporizzato.ino

--- pulsante_temporizzato/pulsante_temporizzato.ino
+++ pulsante_temporizzato/pulsante_temporizzato.ino
@@ -3,10 +3,7 @@ void setup() {
   // put your setup code here, to run once:
     pinMode(11,OUTPUT);
     pinMode(7,INPUT);
-   
-
-
-}
+    }
 
 void loop() {
   // put your main code here, to run repeatedly:
