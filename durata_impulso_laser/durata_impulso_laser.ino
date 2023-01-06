1fe53da7f32c8428ce5c0b8ba75510fbb73e4d04
Oscar Mel <121770935+oscarmel306@users.noreply.github.com>
2023-01-07 00:24:59 +0100

    test tempo di scatto macchina fotografica analogica

--- durata_impulso_laser/durata_impulso_laser.ino
+++ durata_impulso_laser/durata_impulso_laser.ino
@@ -1,4 +1,4 @@
-// test tempo di scatto macchina fotoggrafica analogica
+// misura impulso di un laser in millisecondi
 void setup() {
   Serial.begin(9600);
   pinMode(2, INPUT);
@@ -6,19 +6,27 @@ void setup() {
 
 void loop() {
   float t = pulseIn(2, LOW,60000000);
+  Serial.println("");
+  Serial.println(t);
+  
   if (t == 0) {
     Serial.println("in attesa");
+  } else if (t > 1000000) {
+    t= t/1000000;
+    Serial.print("tempo di scatto: ");
+    Serial.print(t);
+    Serial.println(" sec");
   } else {
   float tmillisec=t;
     tmillisec=tmillisec/1000;
     t=t/1000000;
     t=1/t;
     Serial.print("tempo di scatto: ");
-    Serial.print(" 1/"); 
+    Serial.print("1/"); 
     Serial.print(t);
-    Serial.print(" di secondo; in millisecondi: ");
-    Serial.println(tmillisec);
-    
+    Serial.print(" di secondo  (");
+    Serial.print(tmillisec);
+    Serial.println(" msec)");
 
   }
 }
