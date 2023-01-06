7ea1886e4d924fdc563baf772e061dc136c072bc
Oscar Mel <121770935+oscarmel306@users.noreply.github.com>
2023-01-06 23:24:45 +0100

    Test tempi di scatto

--- durata_impulso_laser/durata_impulso_laser.ino
+++ durata_impulso_laser/durata_impulso_laser.ino
@@ -1,15 +1,16 @@
-
+// misura impulso di un laser in millisecondi
 void setup() {
   Serial.begin(9600);
   pinMode(2, INPUT);
 }
 
 void loop() {
-  unsigned long t = pulseIn(2, HIGH, 3000000);
+  unsigned long t = pulseIn(2, LOW,60000000);
   if (t == 0) {
-    Serial.println("tempo scaduto");
+    Serial.println("in attesa");
   } else {
-    Serial.print("hai premuto per: ");
-    Serial.println(t); 
+    Serial.print("durata impulso: ");
+    Serial.print(t/1000); 
+    Serial.println(" ms");
   }
 }
