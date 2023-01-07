06e69682bc6cb71d2ed44b26c53b4e1b43a776e4
Oscar Mel <121770935+oscarmel306@users.noreply.github.com>
2023-01-07 19:41:56 +0100

    Update Test_velocita_otturatore_con_display.ino

--- Test_velocita_otturatore_con_display/Test_velocita_otturatore_con_display.ino
+++ Test_velocita_otturatore_con_display/Test_velocita_otturatore_con_display.ino
@@ -1,8 +1,7 @@
-// test tempo di scatto macchina fotografica analogica
-// i valori di pulseIn sono in microsecondi 
-// il comando pulseIn fornisce la durata di in impulso. 
+/*test tempo di scatto macchina fotografica analogica
+ il comando di arduino pulseIn fornisce la durata di in impulso in microsecondi. . 
 
-/* utilizzo un display 16x2 per visualizzare i risultati
+ * utilizzo un display 16x2 per visualizzare i risultati
  * LCD RS pin to digital pin 7
  * LCD Enable pin to digital pin 8
  * LCD D4 pin to digital pin 9
@@ -28,21 +27,54 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT);
+  pinMode(2, INPUT); //imposto il pin come input 
+  lcd.begin(16, 2); // inizializzo il display
+
 }
 
 void loop() {
-  float t = pulseIn(2, LOW,60000000);
+  float t = pulseIn(2, LOW,60000000); //se dopo 60 secondi non succede nulla, esco dal comando pulseIn, che mi da' 0
+  float tusec=t;
   Serial.println("");
-  Serial.print(t);
+  Serial.print(t); // visualizzo la durata dell'impulso in microsecondi (tempo di apertura dell'otturatore)
   Serial.println(" microsecondi");
-  if (t == 0) {
+  lcd.setCursor(0,0);
+  lcd.print("                ");
+  lcd.print(tusec);
+  lcd.setCursor(10,0);
+  lcd.print(" usec");
+  
+    // se il comando mi ha dato 0
+  if (t == 0) { 
     Serial.println("in attesa");
-  } else if (t > 1000000) {
+    lcd.setCursor(0, 0);
+    lcd.print("in attesa       ");
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+    
+    
+    // per comodità, se il tempo di scatto è superiore a 1 secondo, riporto il risultato in secondi
+    // altrimenti in frazione
+    
+  } else if (t > 1000000) { 
     t= t/1000000;
     Serial.print("tempo di scatto: ");
     Serial.print(t);
     Serial.println(" sec");
+
+    lcd.setCursor(0,0);
+    lcd.print("                ");
+    lcd.setCursor(0,0);
+    lcd.print(tusec);
+    lcd.setCursor(10,0);
+    lcd.print(" usec");
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0, 1);
+    lcd.print(t);
+    lcd.setCursor(11, 1);
+    lcd.print(" sec");
+    
   } else {
   float tmillisec=t;
     tmillisec=tmillisec/1000;
@@ -55,5 +87,20 @@ void loop() {
     Serial.print(tmillisec);
     Serial.println(" msec)");
 
+    lcd.setCursor(0,0);
+    lcd.print("                ");
+    lcd.setCursor(0,0);
+    lcd.print(tusec);
+    lcd.setCursor(10,0);
+    lcd.print(" usec");
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print("1/");
+    lcd.setCursor(2, 1);
+    lcd.print(t);
+    lcd.setCursor(11, 1);
+    lcd.print(" sec");
   }
+
 }
