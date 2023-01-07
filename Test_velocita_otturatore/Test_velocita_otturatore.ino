d65722373138a3a2e208fb361fafe627812c50ec
Oscar Mel <121770935+oscarmel306@users.noreply.github.com>
2023-01-07 21:38:11 +0100

    velocità otturatore su pc

--- Test_velocita_otturatore/Test_velocita_otturatore.ino
+++ Test_velocita_otturatore/Test_velocita_otturatore.ino
@@ -1,45 +1,21 @@
 /*test tempo di scatto macchina fotografica analogica
- il comando di arduino pulseIn fornisce la durata di in impulso in microsecondi. . 
-
- * utilizzo un display 16x2 per visualizzare i risultati
- * LCD RS pin to digital pin 7
- * LCD Enable pin to digital pin 8
- * LCD D4 pin to digital pin 9
- * LCD D5 pin to digital pin 10
- * LCD D6 pin to digital pin 11
- * LCD D7 pin to digital pin 12
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K trimmer
- * wiper to LCD VO pin (pin 3)
- * 
- * This example code is in the public domain. 
- * http://www.arduino.cc/en/Tutorial/LiquidCrystal
  */
-
-// include the library code:
-#include <LiquidCrystal.h>
-
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-
 
 void setup() {
   Serial.begin(9600);
   pinMode(2, INPUT); //imposto il pin come input 
-  lcd.begin(16, 2); // inizializzo il display
-
 }
 
 void loop() {
-  unsigned long tl = pulseIn(2, LOW,60000000); //se dopo 60 secondi non succede nulla, esco dal comando pulseIn, che mi da' 0
-  unsigned long tusec=tl;
-  float t = (float) tl;
-  Serial.println("");
+  unsigned long t = pulseIn(2, LOW,60000000); //se dopo 60 secondi non succede nulla, esco dal comando pulseIn, che mi da' 0
+  //unsigned long tl = pulseIn(2, LOW,60000000); //se dopo 60 secondi non succede nulla, esco dal comando pulseIn, che mi da' 0
+  //unsigned long tusec=tl;
+  //float t = (float) tl;
+  //Serial.println("");
   Serial.print(t); // visualizzo la durata dell'impulso in microsecondi (tempo di apertura dell'otturatore)
   Serial.println(" microsecondi");
- 
+  Serial.println();
+ /*
   lcd.setCursor(0,0);
   lcd.print("                ");
   lcd.setCursor(0,0);
@@ -106,5 +82,5 @@ void loop() {
     lcd.setCursor(11, 1);
     lcd.print(" sec");
   }
-
+*/
 }
